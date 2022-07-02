0aeaf44672060d2749a8bea5b6330ed825dc2835
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-07-02 15:45:36 -0400

    react-bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import "./App.css"
+import Button from "react-bootstrap/Button"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import NavDropdown from "react-bootstrap/NavDropdown"
+import Container from "react-bootstrap/Container"
 
 function App() {
   return <div className='App'>Hello World</div>
