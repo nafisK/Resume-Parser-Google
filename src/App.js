96f945385ece55e94b932ae5ecbb786b160e07f4
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-07-03 14:00:15 -0400

    added about

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import About from "./components/About"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
       {/* <Router>
         <Routes>
           <Route path='/' element={} />
