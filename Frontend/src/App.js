df27b7f0a08472f4ee0ba8b682e3aa2542b352ba
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-07-04 10:16:03 -0400

    changed layout

---
+++ Frontend/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css"
+import Navbar from "./components/Navbar"
+import Front from "./components/Front"
+import Process from "./components/Process"
+import Support from "./components/Support"
+import AllInOne from "./components/AllInOne"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Front />
+      <Process />
+      <Support />
+      <AllInOne />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
