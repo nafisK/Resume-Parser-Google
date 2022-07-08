f5c76a2b7b83c5ffb110ff4f80ccec07d6073b21
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-07-08 12:04:38 -0400

    added mail-to for admin

--- src/components/TableList.js
+++ src/components/TableList.js
@@ -1,7 +1,13 @@
 import { React } from "react"
 
 export default function TableList({ data }) {
-  console.log(data)
+  function Mailto({ email, subject, body, ...props }) {
+    return (
+      <a href={`mailto:${email}?subject=${subject || ""}&body=${body || ""}`}>
+        {props.children}
+      </a>
+    )
+  }
   return (
     <>
       <tr className='whitespace-nowrap'>
@@ -9,7 +15,11 @@ export default function TableList({ data }) {
           {data.id.substring(1, 5)}
         </td>
         <td className='px-6 py-4'>{data.name}</td>
-        <td className='px-6 py-4'>{data.email}</td>
+        <td className='px-6 py-4'>
+          <Mailto email={data.email} subject='' body=''>
+            {data.email}
+          </Mailto>
+        </td>
         <td>{data.number}</td>
         <td className='px-6 py-4 text-sm text-gray-500'>
           {`${data.created.toDate().getMonth() + 1}/${data.created
@@ -17,7 +27,6 @@ export default function TableList({ data }) {
             .getDate()}/${data.created.toDate().getFullYear()}`}
         </td>
         <td className='px-6 py-4 text-center'>
-          {" "}
           <a href={data.fileUrl} target='_blank'>
             🔗
           </a>
